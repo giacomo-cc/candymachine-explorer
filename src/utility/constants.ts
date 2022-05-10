a7374447808a6044a3d995466f6d733469be6c85
mike-kotikov <kotikov.misha@gmail.com>
2022-05-10 22:35:50 +0300

    remove vue app

--- src/utility/constants.ts
+++
@@ -1,6 +0,0 @@
-export const MAINNET = "https://api.mainnet-beta.solana.com/";
-export const TESTNET = "https://api.testnet.solana.com/";
-export const DEVNET = "https://api.devnet.solana.com/";
-export const LOCALNET = "http://127.0.0.1:8899/";
-
-export const AVAILABLE_NETWORK_ADDRESSES = [MAINNET, TESTNET, DEVNET, LOCALNET];
