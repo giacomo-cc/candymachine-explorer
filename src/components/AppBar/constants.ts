9fea2a373482980c6563405732f36727a3ca8c7b
mike-kotikov <kotikov.misha@gmail.com>
2022-05-13 14:19:30 +0300

    refactor AppBar component

---
+++ src/components/AppBar/constants.ts
@@ -0,0 +1,2 @@
+export const GITHUB_LINK = 'https://github.com/thegreat-vanderlinde/candymachine-explorer'
+export const TWITTER_LINK = 'https://twitter.com/vanderlinde____'
--- src/components/AppBar/index.tsx
+++ src/components/AppBar/index.tsx
@@ -5,34 +5,48 @@ import MuiAppBar from '@mui/material/AppBar'
 import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography'
 import IconButton from '@mui/material/IconButton'
+import Link from '@mui/material/Link'
 
 import GitHubIcon from '@mui/icons-material/GitHub'
 import TwitterIcon from '@mui/icons-material/Twitter'
 
 import theme from '../../configuration/theme'
 
+import { GITHUB_LINK, TWITTER_LINK } from './constants'
+
 const StyledAppBar = styled(MuiAppBar)`
   height: ${theme.spacing(8)};
+  background-color: ${theme.palette.primary.main};
 `
 
-const AppBar = () => {
-  return (
-    <StyledAppBar position="static" color="primary">
-      <Toolbar>
-        <Grid container justifyContent="space-between" alignItems="center">
-          <Typography variant="h6">Candy Machine Explorer</Typography>
-          <Grid item>
-            <IconButton>
-              <GitHubIcon />
-            </IconButton>
-            <IconButton>
-              <TwitterIcon />
-            </IconButton>
-          </Grid>
+const APP_BAR_ITEMS = [
+  {
+    link: GITHUB_LINK,
+    icon: <GitHubIcon />
+  },
+  {
+    link: TWITTER_LINK,
+    icon: <TwitterIcon />
+  }
+]
+
+const AppBar = () => (
+  <StyledAppBar position="static" color="primary">
+    <Toolbar>
+      <Grid container justifyContent="space-between" alignItems="center">
+        <Typography variant="h6">Candy Machine Explorer</Typography>
+        <Grid item>
+          {APP_BAR_ITEMS.map(({ link, icon }) => (
+            <Link key={link} href={link} target="_blank" color="inherit">
+              <IconButton size="large">
+                {icon}
+              </IconButton>
+            </Link>
+          ))}
         </Grid>
-      </Toolbar>
-    </StyledAppBar>
-  )
-}
+      </Grid>
+    </Toolbar>
+  </StyledAppBar>
+)
 
 export default AppBar
