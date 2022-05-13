c79d34739df7d0dc5c92d3bd0bbb0167ecde519f
mike-kotikov <kotikov.misha@gmail.com>
2022-05-13 14:23:53 +0300

    add api call and url id param

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,33 +1,71 @@
-import { useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 
 import Grid from '@mui/material/Grid'
 
 import { AVAILABLE_NETWORK_ADDRESSES } from '../../configuration/availableNetworkAddresses'
+import { fetchCandyMachineState } from '../../api/fetchCandyMachineState'
+import { CandyMachineState } from '../../api/fetchCandyMachineState/types'
 
-import AppBar from '../AppBar'
 import CandyMachineCard from '../CandyMachineCard'
+import CandyMachineDetails from '../CandyMachineDetails'
+import AppBar from '../AppBar'
 
 import { FormChangeEvent, FormData } from './types'
 
-const Home = () => {
-  const [formData, setFormData] = useState<FormData>({
-    cluster: AVAILABLE_NETWORK_ADDRESSES[0],
-    candyMachineId: ''
-  })
+const PAGE_URL = new URL(window.location.href)
+const INITIAL_FORM_DATA = {
+  cluster: AVAILABLE_NETWORK_ADDRESSES[0],
+  candyMachineId: PAGE_URL.searchParams.get('id') || ''
+}
 
-  const onInputChange = ({ target }: FormChangeEvent) => {
-    setFormData({ ...formData, [target.name]: target.value })
-  }
+const Home = () => {
+  const [isLoading, setIsLoading] = useState(false)
+  const [details, setDetails] = useState<CandyMachineState>()
+  const [formData, setFormData] = useState<FormData>(INITIAL_FORM_DATA)
+
+  useEffect(() => {
+    if (formData.candyMachineId) {
+      setIsLoading(true)
+      ;(async () => {
+        try {
+          const result = await fetchCandyMachineState(
+            formData.cluster || '',
+            formData.candyMachineId || ''
+          )
+          setDetails(result)
+        } catch (e) {
+          console.error(e)
+        } finally {
+          setIsLoading(false)
+        }
+      })()
+    }
+  }, [formData])
+
+  const onInputChange = useCallback(
+    ({ target }: FormChangeEvent): void => {
+      const { name, value } = target
+      setFormData({ ...formData, [name]: value })
+
+      if (name === 'candyMachineId') {
+        PAGE_URL.searchParams.set('id', value)
+        window.history.pushState(null, '', PAGE_URL)
+      }
+    },
+    [formData]
+  )
 
   return (
-    <Grid container>
+    <Grid container justifyContent="center">
       <AppBar />
       <CandyMachineCard
         cluster={formData.cluster}
         candyMachineId={formData.candyMachineId}
         onClusterChange={onInputChange}
         onCandyMachineIdChange={onInputChange}
+        isLoading={isLoading}
       />
+      <CandyMachineDetails cluster={formData.cluster || ''} data={details || null} />
     </Grid>
   )
 }
